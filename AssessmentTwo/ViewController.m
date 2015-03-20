e5a315a332a48563c41186f4ec4a785d7e64db5f
mertakanay <mert.akanay@live.com>
2015-03-20 09:08:27 -0700

    Initial Commit

---
+++ AssessmentTwo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AssessmentTwo
+//
+//  Created by Mert Akanay on 20.03.2015.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
